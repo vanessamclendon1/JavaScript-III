5ef9642ee8c1f0f41d91177b81157d9e07668c85
Vanessa McLendon <vanessamclendon@Vanessas-MacBook-Pro-2.local>
2019-02-27 15:12:18 -0500

    first commit

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,11 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding - happens when in the global scope.
+* 2. Implicit Binding - happens whenever a function is called by a preceding dot. 
+* 3. New binding - happens when a constructor function is used this refers to 
+    instance of the Object that is created and returned by the constructor function
+* 4. Explicit binding - happens when the JavaScript call or apply method is used.
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +13,7 @@
 // Principle 1
 
 // code example for Window Binding
+
 
 // Principle 2
 
